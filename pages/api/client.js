b4716826b5793a386c7a4eee3fed3ebc3e03460b
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-12-01 21:57:32 -0300

    Configuring the Identity Pool to access our Topic

--- pages/api/client.js
+++ pages/api/client.js
@@ -1,5 +1,16 @@
 import { SNSClient } from "@aws-sdk/client-sns";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "../../node_modules/@aws-sdk/credential-provider-cognito-identity";
 
+// Set the AWS Region.
+const snsClient = new SNSClient({
+  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  credentials: fromCognitoIdentityPool({
+    client: new CognitoIdentityClient({
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+    }),
+    identityPoolId: process.env.NEXT_PUBLIC_AWS_INDENTITY_POOL, // IDENTITY_POOL_ID
+  }),
+});
 // Create SNS service object.
-const snsClient = new SNSClient({ region: process.env.NEXT_PUBLIC_AWS_REGION });
 export { snsClient };
