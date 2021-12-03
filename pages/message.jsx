0d949b4244e6a82d21eff856abcaee2ee227433b
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-12-02 21:28:24 -0300

    Sending messages to subscribes

--- pages/message.jsx
+++ pages/message.jsx
@@ -1,12 +1,44 @@
+import { PublishCommand } from "@aws-sdk/client-sns";
+import { snsClient } from "./api/client.js";
+
 export default function Message() {
+  async function SendMessage() {
+    const message = document.getElementById("message").value;
+    const subject = document.getElementById("subject").value;
+
+    var params = {
+      Subject: subject,
+      Message: message, // MESSAGE_TEXT
+      TopicArn: process.env.NEXT_PUBLIC_AWS_TOPIC_ARN, //TOPIC_ARN
+    };
+
+    try {
+      const data = await snsClient.send(new PublishCommand(params));
+      alert("Message sent successfully");
+      document.getElementById("message").value = "";
+      document.getElementById("subject").value = "";
+      console.log("Success.", data);
+      return data; // For unit tests.
+    } catch (err) {
+      alert("Error sending message");
+      console.log("Error", err.stack);
+    }
+  }
+
   return (
     <div className="main">
       <h1>Write your message</h1>
-      <textarea cols="50" rows="2" placeholder="Subject" type="text" />
+      <textarea
+        cols="50"
+        rows="2"
+        placeholder="Subject"
+        type="text"
+        id="subject"
+      />
       <br />
       <textarea cols="50" rows="10" placeholder="Message" id="message" />
       <br />
-      <button>Send Message</button>
+      <button onClick={() => SendMessage()}>Send Message</button>
     </div>
   );
 }
